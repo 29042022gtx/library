0a5cb10eaf9e2ad27344730fe690f2ca54b1fb59
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-07-21 22:13:26 +0700

    Add dialog

--- script.js
+++ script.js
@@ -13,30 +13,30 @@ function populateBooks() {
   // })
   
   myLibrary.forEach(obj => {
-    const div = newDiv();
-
-    let content = newDiv('title');
-    content.textContent = obj.title;
-    div.appendChild(content);
-
-    content = newDiv('author');
-    content.textContent = obj.author;
-    div.appendChild(content);
-
-    content = newDiv('pages');
-    content.textContent = obj.pages + ' pages';
-    div.appendChild(content);
-
-    content = newDiv('read');
-    content.textContent = obj.read ? 'Read' : 'Not read';
-    div.appendChild(content);
-
-    container.prepend(div);
+    pushBook(obj);
   })
 }
 
-function pushContent() {
+function pushBook(obj) {
+  const div = newDiv();
 
+  let content = newDiv('title');
+  content.textContent = obj.title;
+  div.appendChild(content);
+
+  content = newDiv('author');
+  content.textContent = obj.author;
+  div.appendChild(content);
+
+  content = newDiv('pages');
+  content.textContent = obj.pages + ' pages';
+  div.appendChild(content);
+
+  content = newDiv('read');
+  content.textContent = obj.read ? 'Read' : 'Not read';
+  div.appendChild(content);
+
+  container.prepend(div);
 }
 
 function newDiv(className) {
@@ -46,7 +46,11 @@ function newDiv(className) {
   return element;
 }
 
+const dialog = document.querySelector('dialog');
 const container = document.querySelector('.container');
+const form = document.querySelector('.dialog form');
+const plus = document.querySelector('.container #plus');
+const add = document.querySelector('.dialog #add');
 const book1 = new Book('Abc', 'Game of thrones', 600, true);
 const book2 = new Book('Xyz', 'Dune', 525, false);
 const book3 = new Book(1, '2', 3, false);
@@ -56,4 +60,26 @@ const myLibrary = [book1, book2, book3, book4,
   book1, book2
 ];
 
+plus.addEventListener('click', () => {
+  dialog.showModal();
+})
+
+add.addEventListener('click', e => {
+  e.preventDefault();
+  const title = form.title.value;
+  const author = form.author.value;
+  const pages = form.pages.value;
+  const book = new Book(title, author, pages, false);
+  pushBook(book);
+  myLibrary.push(book);
+  dialog.close();
+})
+
 populateBooks();
+
+// const cancel = document.querySelector('#cancel');
+
+// cancel.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   dialog.close();
+// });
