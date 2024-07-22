50d0c36b30917fcafb5e9abad2f40ef411c0df77
Owen <150531389+29042022gtx@users.noreply.github.com>
2024-07-22 10:43:09 +0700

    Add delete button

--- script.js
+++ script.js
@@ -5,21 +5,26 @@ function Book(author, title, pages, read) {
   this.read = read;
 }
 
+function clearContainer() {
+  for (let i = container.childNodes.length - 3; i > 1; i--)
+    container.removeChild(container.firstChild);
+}
+
 function populateBooks() {
   // myLibrary.sort((a, b) => {
   //   let s1 = a.title;
   //   let s2 = b.title;
   //   return s1.localeCompare(s2, undefined, {sensitivity: 'accent'});
   // })
-  
+  let idx = 0;
   myLibrary.forEach(obj => {
-    pushBook(obj);
+    prependBook(obj, idx++);
   })
 }
 
-function pushBook(obj) {
+function prependBook(obj, idx) {
   const div = newDiv();
-
+  div.setAttribute('data-idx', idx);
   let content = newDiv('title');
   content.textContent = obj.title;
   div.appendChild(content);
@@ -34,6 +39,10 @@ function pushBook(obj) {
 
   content = newDiv('read');
   content.textContent = obj.read ? 'Read' : 'Not read';
+  div.appendChild(content);
+
+  content = newDiv('delete');
+  content.textContent = 'x';
   div.appendChild(content);
 
   container.prepend(div);
@@ -56,8 +65,8 @@ const book2 = new Book('Xyz', 'Dune', 525, false);
 const book3 = new Book(1, '2', 3, false);
 const book4 = new Book('Abc', 'Game of thrones', 600, true);
 const myLibrary = [book1, book2, book3, book4,
-  book1, book2, book3, book4,
-  book1, book2
+  // book1, book2, book3, book4,
+  // book1
 ];
 
 plus.addEventListener('click', () => {
@@ -70,9 +79,21 @@ add.addEventListener('click', e => {
   const author = form.author.value;
   const pages = form.pages.value;
   const book = new Book(title, author, pages, false);
-  pushBook(book);
+  prependBook(book);
   myLibrary.push(book);
   dialog.close();
+})
+
+container.addEventListener('click', e => {
+  // HTMLElement.prototype.getAttribute('data-idx')
+  if (e.target.classList.contains('delete')) {
+    const book = e.target.parentElement;
+    book.remove();
+    const idx = +book.getAttribute('data-idx')
+    myLibrary.splice(idx, 1);
+    clearContainer();
+    populateBooks();
+  }
 })
 
 populateBooks();
